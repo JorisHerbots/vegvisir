b8677f9b3242d8dc6507476da45f041058223869
Mike Vandersanden <mike.vandersanden@student.uhasselt.be>
2021-08-05 15:01:39 +0200

    make runner compatible with dockerhub image

--- vegvisir/runner.py
+++ vegvisir/runner.py
@@ -4,9 +4,18 @@ import sys
 import subprocess
 from typing import List
 import json
+import tempfile
+import re
+import shutil
 
 from .implementation import Implementation, Role
-from .testcase import TestResult
+from .testcase import Status, TestCase, TestResult
+
+class LogFileFormatter(logging.Formatter):
+	def format(self, record):
+		msg = super(LogFileFormatter, self).format(record)
+		# remove color control characters
+		return re.compile(r"\x1B[@-_][0-?]*[ -/]*[@-~]").sub("", msg)
 
 class Runner:
 	_start_time: datetime = 0
@@ -18,6 +27,8 @@ class Runner:
 
 	_logger: logging.Logger = None
 	_debug: bool = False
+	_save_files: bool = False
+	_log_dir: str = ""
 
 	def __init__(
 		self,
@@ -70,6 +81,7 @@ class Runner:
 
 	def run(self) -> int:
 		self._start_time = datetime.now()
+		self._log_dir = "logs_{:%Y-%m-%dT%H:%M:%S}".format(self._start_time)
 		nr_failed = 0
 
 		for shaper in self._shapers:
@@ -81,7 +93,10 @@ class Runner:
 					client.name, client.image
 					)
 
-					result = self._run_test(shaper, server, client)
+					testcase = TestCase()
+					testcase.name = "test"
+
+					result = self._run_test(shaper, server, client, testcase)
 					logging.debug("\telapsed time since start of test: %s", str(result.end_time - result.start_time))
 
 		self._end_time = datetime.now()
@@ -92,19 +107,43 @@ class Runner:
 		self,
 		shaper: Implementation,
 		server: Implementation, 
-		client: Implementation
+		client: Implementation,
+		testcase: TestCase
 		) -> TestResult:
 		result = TestResult()
 		result.start_time = datetime.now()
 
+		sim_log_dir = tempfile.TemporaryDirectory(dir="/tmp", prefix="logs_sim_")
+		server_log_dir = tempfile.TemporaryDirectory(dir="/tmp", prefix="logs_server_")
+		client_log_dir = tempfile.TemporaryDirectory(dir="/tmp", prefix="logs_client_")
+		log_file = tempfile.NamedTemporaryFile(dir="/tmp", prefix="output_log_")
+		log_handler = logging.FileHandler(log_file.name)
+		log_handler.setLevel(logging.DEBUG)
+
+		formatter = LogFileFormatter("%(asctime)s %(message)s")
+		log_handler.setFormatter(formatter)
+		logging.getLogger().addHandler(log_handler)
+
 		params = (
+			"WAITFORSERVER=server:443 "
+
 			"CLIENT=" + client.image + " "
-			"CLIENT_PARAMS=\"--ca-certs tests/pycacert.pem https://193.167.100.100:4433/\"" + " "
-			"DOWNLOADS=" + "/" + " "
+			"TESTCASE_CLIENT=transfer" + " "
+			"CLIENT_PARAMS=\"--ca-certs tests/pycacert.pem -q /logs/qlog\"" + " "
+			"REQUESTS=\"https://193.167.100.100:443/\"" + " "
+
+			"DOWNLOADS=" + "./downloads" + " "
 			"SERVER=" + server.image + " "
-			"SERVER_PARAMS=\"--certificate tests/ssl_cert.pem --private-key tests/ssl_key.pem\"" + " "
+			"TESTCASE_SERVER=" + " "
+			"SERVER_PARAMS=\"--certificate tests/ssl_cert.pem --private-key tests/ssl_key.pem -q /logs/qlog\"" + " "
+			"WWW=" + "./www" + " "
+			"CERTS=" + "./certs" + " "
+
 			"SHAPER=" + shaper.image + " "
 			"SCENARIO=\"simple-p2p --delay=15ms --bandwidth=10Mbps --queue=25\"" + " "
+
+			"SERVER_LOGS=" + "/logs" + " "
+			"CLIENT_LOGS=" + "/logs" + " "
 		)
 		containers = "sim client server"
 
@@ -114,7 +153,9 @@ class Runner:
 			+ containers
 		)
 
+		result.status = Status.FAILED
 		try:
+			logging.debug("running command: %s", cmd)
 			proc = subprocess.run(
 				cmd,
 				shell=True,
@@ -123,10 +164,51 @@ class Runner:
 				timeout=30
 			)
 			logging.debug("proc: %s", proc.stdout.decode("utf-8"))
+			result.status = Status.SUCCES
 		except subprocess.TimeoutExpired as e:
 			logging.debug("subprocess timeout: %s", e.stdout.decode("utf-8"))
 		except Exception as e:
 			logging.debug("subprocess error: %s", str(e))
 
+		self._copy_logs("sim", sim_log_dir)
+		self._copy_logs("client", client_log_dir)
+		self._copy_logs("server", server_log_dir)
+
+		# save logs
+		logging.getLogger().removeHandler(log_handler)
+		log_handler.close()
+		if result.status == Status.FAILED or result.status == Status.SUCCES:
+			log_dir = self._log_dir + "/" + server.name + "_" + client.name + "/" + testcase.name
+			shutil.copytree(server_log_dir.name, log_dir + "/server")
+			shutil.copytree(client_log_dir.name, log_dir + "/client")
+			shutil.copytree(sim_log_dir.name, log_dir + "/sim")
+			shutil.copyfile(log_file.name, log_dir + "/output.txt")
+			if self._save_files and result.status == Status.FAILED:
+				shutil.copytree(testcase.www_dir(), log_dir + "/www")
+				try:
+					shutil.copytree(testcase.download_dir(), log_dir + "/downloads")
+				except Exception as exception:
+					logging.info("Could not copy downloaded files: %s", exception)
+
+		server_log_dir.cleanup()
+		client_log_dir.cleanup()
+		sim_log_dir.cleanup()
+
 		result.end_time = datetime.now()
 		return result
+
+
+	def _copy_logs(self, container: str, dir: tempfile.TemporaryDirectory):
+		r = subprocess.run(
+			'docker cp "$(docker-compose --log-level ERROR ps -q '
+			+ container
+			+ ')":/logs/. '
+			+ dir.name,
+			shell=True,
+			stdout=subprocess.PIPE,
+			stderr=subprocess.STDOUT,
+		)
+		if r.returncode != 0:
+			logging.info(
+				"Copying logs from %s failed: %s", container, r.stdout.decode("utf-8")
+			)
--- vegvisir/testcase.py
+++ vegvisir/testcase.py
@@ -1,8 +1,32 @@
 from datetime import date, datetime
+from enum import Enum
+import tempfile
+
+class Status(Enum):
+	SUCCES = 1
+	FAILED = 2
 
 class TestResult:
 	start_time: datetime = 0
 	end_time: datetime = 0
+	status: Status = Status.FAILED
 
 	def __init__(self):
 		pass
+
+class TestCase:
+	name: str= ""
+	_www_dir = None
+	_download_dir = None
+
+	def www_dir(self):
+		if not self._www_dir:
+			self._www_dir = tempfile.TemporaryDirectory(dir="/tmp", prefix="www_")
+		return self._www_dir.name + "/"
+
+	def download_dir(self):
+		if not self._download_dir:
+			self._download_dir = tempfile.TemporaryDirectory(
+				dir="/tmp", prefix="download_"
+			)
+		return self._download_dir.name + "/"
