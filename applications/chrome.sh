da79e47219d2a4ff5dbda20a2c2ee53a39e85d46
Mike Vandersanden <mike.vandersanden@student.uhasselt.be>
2021-09-16 11:49:54 +0200

    revert usage of chrome starter, instead change command

--- applications/chrome.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-$1 --origin-to-force-quic-on="$2" --enable-experimental-web-platform-features --autoplay-policy=no-user-gesture-required --auto-open-devtools-for-tabs --ignore-certificate-errors-spki-list="$3" "https://$2/dashjs-qlog-abr/demo/demo.html?autosave=true&video=$4"
