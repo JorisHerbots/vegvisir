a1c48f5daaace6a36450ac1ca65b55729fdfaedd
olaf <olaf.vanbylen@student.uhasselt.be>
2022-09-19 12:13:15 +0200

    Added no password error for import button

--- vegvisirweb/src/views/HomeView.vue
+++ vegvisirweb/src/views/HomeView.vue
@@ -2,7 +2,15 @@
   <main>
 
     <ImportTestModal v-if="importModalVisible" @close="importModalVisible = false"></ImportTestModal>
-
+    <div v-if="showError">
+      <div id="alert" class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded z-40" role="alert">
+        <strong class="font-bold">Error</strong>
+        <span class="block sm:inline ml-4">{{errorMessage}}</span>
+        <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
+          <svg @click="showError = false;" class="fill-current h-6 w-6 text-red-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/></svg>
+        </span>
+      </div>
+    </div>
 
       <div class="text-center text-5xl font-medium mt-8 mb-32">
         Welcome to Vegvisir!
@@ -12,7 +20,7 @@
     Vegvisir needs your sudo password to work, please enter it <br>
   <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded m-4 py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-teal-500 z-1" v-model="password" type="password" id="inline-full-name" placeholder="password">
     
-    <button @click="sendPassword" class=" mt-4 mb-8 bbg-transparent hover:bg-teal-500 text-teal-500 font-semibold hover:text-white py-2 px-4 border border-teal-500 hover:border-transparent rounded">
+    <button @click="sendPassword" class="mt-4 mb-8 bbg-transparent hover:bg-teal-500 text-teal-500 font-semibold hover:text-white py-2 px-4 border border-teal-500 hover:border-transparent rounded">
         Submit
     </button>
   </div>
@@ -40,7 +48,7 @@
 
       <div class="text-center text-xl font-medium mt-8 flex-1">
         <div class="text-3xl">Would you like to import and reproduce a test? <br></div>
-        <button @click="importModalVisible = true" class=" mt-4 mb-8 bbg-transparent hover:bg-teal-500 text-teal-500 font-semibold hover:text-white py-2 px-4 border border-teal-500 hover:border-transparent rounded">
+        <button @click="if (checkError()){} else {importModalVisible = true};" class=" mt-4 mb-8 bbg-transparent hover:bg-teal-500 text-teal-500 font-semibold hover:text-white py-2 px-4 border border-teal-500 hover:border-transparent rounded">
             Import Test
         </button>
 
@@ -91,11 +99,13 @@ export default {
   setup() {
     const passwordStore = usePasswordStore();
     const importModalVisible = ref<Boolean>(false);
+    const showError = ref<Boolean>(false);
 
-    return { passwordStore, importModalVisible }
+    return { passwordStore, importModalVisible, showError }
   },
   data: () => ({
-    password: String
+    password: String,
+    errorMessage: String
     }),
    created() {
     axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
@@ -104,6 +114,16 @@ export default {
   methods: {
     sendPassword() {
       this.passwordStore.setPassword(this.password);
+      this.showError = false;
+    },
+    checkError() {
+      if (!this.passwordStore.password_is_set)
+      {
+        this.errorMessage = "Please fill in and submit your sudo password on the home page"
+        this.showError = true;
+        return true;
+      }
+      return false;      
     }
   }
 }
